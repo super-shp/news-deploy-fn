aa11bf62912cfe905aacc18553646f7d6816dfeb
caoyong <caoyong@bytedance.com>
2018-07-02 23:37:33 +0800

    add editorconfig
    Change-Id: Ib6e62512096f7ee136730965afffe5a16451723c

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Test ts for news project"/>
   </div>
 </template>
 
