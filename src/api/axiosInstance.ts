c92fb58ed9d91db6547774c1d1724b380bacedc0
caoyong <caoyong@bytedance.com>
2018-07-10 12:07:56 +0800

    fix bug of baseURL
    Change-Id: I2af68ec50791415d6279f875272a8ad599394506

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import { default as axios, AxiosInstance } from 'axios';
 
-const baseURL = 'localhost:8080/';
+const baseURL = 'http://localhost:8080/';
 
 export const api: AxiosInstance = axios.create({
   baseURL,
