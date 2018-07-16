cea0e60bc101bf39797d31f8c63c443ff160bcd9
caoyong <caoyong@bytedance.com>
2018-07-16 20:18:58 +0800

    fix router guard
    Change-Id: I90990255095718caa5eb14f2cf55f5348492654c

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -1,6 +1,6 @@
 import { NavigationGuard } from 'vue-router';
 
 export const beforeEach: NavigationGuard = (to, from, next) => {
-  // judge for auth
-  next(to.path);
+  // some intercept
+  next();
 };
